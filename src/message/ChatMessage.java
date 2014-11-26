013513b362ad1018fb69c1fefb30b685a4594e87
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame14-Th>
2014-11-26 15:01:14 +0200

    Initial commit. Library example.

---
+++ src/message/ChatMessage.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import java.io.Serializable;
+
+/**
+ * This class can be used as serializable object between the client
+ * and server in chat program.
+ * 
+ * @author Kimmo T.
+ */
+public class ChatMessage implements Serializable {
+    
+    private String userName; // sendder of the message
+    private String chatMessage;
+    private String messageColor;
+    private int fontSize; // range 12 to 25
+    private boolean isPrivate;
+    private String privateName; // receiver of the private message
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getChatMessage() {
+        return chatMessage;
+    }
+
+    public void setChatMessage(String chatMessage) {
+        this.chatMessage = chatMessage;
+    }
+
+    public String getMessageColor() {
+        return messageColor;
+    }
+
+    public void setMessageColor(String messageColor) {
+        this.messageColor = messageColor;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        if (fontSize >= 12 || fontSize < 26) {
+            this.fontSize = fontSize;
+        }
+    }
+
+    public boolean isIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public String getPrivateName() {
+        return privateName;
+    }
+
+    public void setPrivateName(String privateName) {
+        this.privateName = privateName;
+    }
+    
+    
+}
